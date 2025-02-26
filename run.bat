8454132b60c213e7003f55e924d60a317bfd8bde
ziyii <temp@ziyii.top>
2025-02-26 22:07:10 +0800

    ✨ feat(run.bat): 隐藏bat界面运行脚本。

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
+@echo off
+if "%1"=="h" goto begin
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
+:begin
+REM 你的批处理文件内容
 call .venv/Scripts/activate.bat
 python main.py
-pause
